89ff856bd65b197fd9772717040416fa082d0534
EAG <rauulssanchezz@gmail.com>
2023-10-18 20:32:43 +0200

    aa

--- app/src/main/java/com/example/seminario/main.kt
+++ app/src/main/java/com/example/seminario/main.kt
@@ -13,6 +13,9 @@ fun main(){
     println(Mayuscula("me miro el pito iubiubh pito"))
     println(SumaCifras(123))
     println(Mcd(24,40))
+    println(Fibonacci(2))
+    println(PrimosRelativos(10,20))
+    println(Capicua(122))
 }
 //Ejercicio1
 fun Maximo(array:Array<Int>):Int{
@@ -124,3 +127,57 @@ fun Mcd(numero: Int,numero2:Int):Int{
     }
     return res
 }
+//Ejercicio10
+fun Fibonacci(numero:Int):Int{
+    var res=0
+    var n1 = 0
+    var n2 = 1
+
+    if (numero == 0) return 0
+    if (numero == 1) return 1
+    if (numero == 2) return 1
+
+    for(n in 3..numero){
+        res=n1+n2
+        n1=n2
+        n2=res
+    }
+
+    return res
+}
+
+//Ejercicio11
+fun PrimosRelativos(n1:Int,n2:Int):Boolean{
+
+    var fact1=0
+    var fact2=0
+
+    for(n in 1..n1){
+        if(n1%n==0){
+            fact1=n
+        }
+    }
+    for(n in 1..n2){
+        if(n1%n==0){
+            fact2=n
+        }
+    }
+    if(fact1!=0 && fact2!=0){
+        if(fact1==fact2){
+            return false
+        }
+    }
+    return true
+}
+
+//Ejercicio12
+fun Capicua(numero:Int):Boolean{
+    var n1=numero
+    var n2=numero.toString().reversed().toInt()
+    if(n1==n2){
+        return true
+    }
+    return false
+}
+//Ejercio13
+fun
