2f425f54bcb6d78532d5aac5a3b7def6cf92ff57
EAG <rauulssanchezz@gmail.com>
2023-10-19 18:48:40 +0200

    aa

--- app/src/main/java/com/example/seminario/main.kt
+++ app/src/main/java/com/example/seminario/main.kt
@@ -4,6 +4,8 @@ import kotlin.system.exitProcess
 
 fun main(){
     var array= arrayOf(7,3,80,75,3,1)
+    var array1= arrayOf(5,4,3,2,9,8)
+    var matriz = arrayOf(arrayOf(1,20,3,2,1), arrayOf(2,6,5,3,9))
     println(Maximo(array))
     println(Sumatoria(array))
     println(Kilometros(100.0))
@@ -16,6 +18,22 @@ fun main(){
     println(Fibonacci(2))
     println(PrimosRelativos(10,20))
     println(Capicua(122))
+    println(Etiqueta("a.kaka"))
+    print(Mosaico(5))
+    for(i in array.indices){
+        print(CompararArrays(array,array1)[i])
+    }
+    println()
+    println(Factorial(5))
+    println(Reverso("Hola"))
+    println(Perfecto(6))
+    println(Amstrong(153))
+    println(EncontrarMayor(matriz))
+    println(EncontrarMenor(matriz))
+    println(PalabraLarga("Hola bebee como te va como te va"))
+    println(PalabraCorta("Hola bebee como te va como te va"))
+    println(Determinar("Por el amor de es2a mujeeeer"))
+    println(Anagrama("pene","nepe"))
 }
 //Ejercicio1
 fun Maximo(array:Array<Int>):Int{
@@ -180,4 +198,162 @@ fun Capicua(numero:Int):Boolean{
     return false
 }
 //Ejercio13
-fun
+fun Etiqueta(texto:String):String{
+    val etiqueta = texto.split(".")
+    var res=""
+    res= "<"+etiqueta[0]+" class= "+etiqueta[1]+"></"+etiqueta[0]+">"
+    return res
+}
+
+//Ejercicio 14
+fun Mosaico(n:Int):String{
+    var res=""
+    for(i in 1..n){
+        for(z in 1..i){
+            res+=i
+
+        }
+        res+="\n"
+    }
+    return res
+}
+//Ejercicio15
+fun CompararArrays(array1:Array<Int>,array2:Array<Int>):BooleanArray{
+    var arraybo = BooleanArray(array1.size)
+    for(i in array1.indices){
+        if(array1[i]==array2[i]){
+            arraybo[i]=true
+        }
+    }
+    return arraybo
+}
+
+//Ejercicio20
+fun Factorial(n:Int):Int{
+    var res=1
+    for(i in 1..n){
+        res*=i
+    }
+    return res
+}
+
+//Ejercicio21
+fun Reverso(texto:String):String{
+    return texto.reversed()
+}
+
+//Ejercicio22
+fun Perfecto(n:Int):Boolean{
+    var res=0
+    for(i in 1..n){
+        if(n%i==0 && n!=i){
+            res+=i
+        }
+    }
+    if(res==n){
+        return true
+    }
+    return false
+}
+
+//Ejercicio 23
+fun Amstrong (n:Int):Boolean{
+    var res=0
+    var cifra=0
+    var numero=n
+    for(i in n.toString()){
+        cifra = numero%10
+        numero=numero/10
+        res+= Math.pow(cifra.toDouble(), n.toString().length.toDouble()).toInt()
+    }
+    if(res==n){
+        return true
+    }
+    return false
+}
+
+//Ejercicio 24
+fun EncontrarMayor(matriz:Array<Array<Int>>):Int{
+    var res=matriz[0][0]
+    for(i in matriz.indices){
+        for(z in matriz[0].indices){
+            if(matriz[i][z]>res){
+                res=matriz[i][z]
+            }
+        }
+    }
+    return res
+}
+
+//Ejercicio25
+fun EncontrarMenor(matriz:Array<Array<Int>>):Int{
+    var res=matriz[0][0]
+    for(i in matriz.indices){
+        for(z in matriz[0].indices){
+            if(matriz[i][z]<res){
+                res=matriz[i][z]
+            }
+        }
+    }
+    return res
+}
+
+//Ejercicio26
+fun PalabraLarga(text:String):String{
+    var palabras=text.split(" ")
+    var res=palabras[0]
+    for(i in palabras.indices){
+        if(palabras[i].length>res.length){
+            res=palabras[i]
+        }
+    }
+    return res
+}
+
+//Ejercicio27
+fun PalabraCorta(text:String):String{
+    var palabras=text.split(" ")
+    var res=palabras[0]
+    for(i in palabras.indices){
+        if(palabras[i].length<res.length){
+            res=palabras[i]
+        }
+    }
+    return res
+}
+
+//Ejercicio28
+fun Determinar(text:String):Boolean{
+    var lista=text.split("")
+    for(i in lista.indices){
+        for(z in 0..9) {
+            if (lista[i].equals(z.toString())) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+//Ejercicio29
+fun Anagrama(text:String,text2:String):Boolean{
+    var confirmar= BooleanArray(text.length)
+
+    var z = 0
+    loop1@ for (i in text.indices){
+       loop2@ for(z in text2.indices){
+            if(text[i].equals(text2[z])){
+                confirmar[i]=true
+                continue@loop1
+            }else{
+                confirmar[i]=false
+            }
+        }
+    }
+    for(i in confirmar.indices){
+        if(confirmar[i]==false){
+            return false
+        }
+    }
+    return true
+}
